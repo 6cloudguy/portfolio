3b5cc409de0bbebdcbc767487db942071d5b5a94
6cloudguy <pranav2308836@gmail.com>
2026-07-07 22:59:50 +0530

    removed pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
-import Projects from './pages/Projects';
-import Writeups from './pages/Writeups';
-import Contact from './pages/Contact';
 import NotFound from './pages/NotFound';
 
 function App() {
@@ -12,9 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="projects" element={<Projects />} />
-          <Route path="writeups" element={<Writeups />} />
-          <Route path="contact" element={<Contact />} />
         </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
